c1c54fae059d1d1015dcf211ce1df4dbb6049d1d
wideputin117 <takermanish7@gmail.com>
2023-12-29 22:51:37 +0530

    exp1

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -51,7 +51,7 @@ export async function POST(req: Request) {
   }
  
   // Get the ID and type
-  const { id } = evt.data;
+ //s const { id } = evt.data;
   const eventType = evt.type;
  
   if(eventType === 'user.created') {
