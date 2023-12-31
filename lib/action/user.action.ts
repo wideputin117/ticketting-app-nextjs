8bcf0a726bfc4d387c00e7955de98cfe83311a00
wideputin117 <takermanish7@gmail.com>
2024-01-01 02:33:11 +0530

    new_updates

--- lib/action/user.action.ts
+++ lib/action/user.action.ts
@@ -16,7 +16,8 @@ export async function createUser(user: CreateUserParams) {
   try {
     await connectToDatabase()
 
-    const newUser = await User.create(user)
+    const newUser = await User.create(user);
+    console.log(newUser);
     return JSON.parse(JSON.stringify(newUser))
   } catch (error) {
     handleError(error)
