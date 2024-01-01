1c07cce80ec4a1813738d015a6e2aaf43937da28
wideputin117 <takermanish7@gmail.com>
2024-01-01 16:17:11 +0530

    latest

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -5,13 +5,13 @@ const MONGODB_URI = process.env.MONGODB_URI;
 // caching the connection 
 let cached = (global as any).mongoose || {conn: null, promise: null} ;
 
-export const connectToDatabase =async () => {
-    if(cached.conn) return cached.conn; // checks for an cached connection i.e if the connection is cached 
+export const connectToDatabase = async () => {
+    if (cached.conn) return cached.conn; // checks for an cached connection i.e if the connection is cached 
     if(!MONGODB_URI){                   // check for connection string
         throw new Error('MongoDB_URI is missing');
     }
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, 
-{bufferCommands: false, })
+
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, { bufferCommands: false, })
 
 // assigning cached.promise to cached.conn and returning it;
 cached.conn = await cached.promise;
