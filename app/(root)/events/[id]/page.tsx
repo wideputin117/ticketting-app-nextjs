5eb7a76abf7fe028e12ff1846daf855a87507726
wideputin117 <takermanish7@gmail.com>
2024-01-08 02:02:59 +0530

    updated next,config for letting the images from uploadthing

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -4,13 +4,14 @@ import { getEventById } from '@/lib/action/event.actions'
 import { SearchParamProps } from '@/public/types'
 import React from 'react'
 
-const EventDetails =async ({params:{ id }}: SearchParamProps) => {
+const EventDetails =async ({ params:{ id }}: SearchParamProps) => {
     const event = await getEventById(id);
+    console.log(event);
     return(
         <>
-          <div>
-            My details
-          </div>
+           <section className='flex justify-center bg-primary-50 bg-dotted-pattern'>
+
+           </section>
         
         </>
     )
